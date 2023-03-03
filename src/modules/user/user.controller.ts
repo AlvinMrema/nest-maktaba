647a3618763155712378811eaeba7075f8fe4ea1
AlvinMrema <sonalpha023@gmail.com>
2023-03-03 15:50:49 +0300

    Added User related operations to API

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,29 +1,42 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Post()
-  createUser(userData) {
-    console.log('User created', userData);
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.createUser(createUserDto);
   }
 
   @Get()
-  getUsers() {
-    console.log('All Users.');
+  async getUsers() {
+    return await this.userService.getUsers();
   }
 
   @Get(':id')
-  getUserById(@Param() id) {
-    console.log(`User with ID -> ${id}`);
+  async getUserById(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.getUserById(id);
   }
 
   @Put(':id')
-  updateUser(@Param('id') id: string, userData) {
+  updateUser(@Param('id', ParseIntPipe) id: number, userData) {
     console.log(`User ${id} updated`, userData);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id') id: string) {
-    console.log(`User ${id} Deleted`);
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = new User();
+    user.username = createUserDto.username;
+    user.email = createUserDto.email;
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(createUserDto.password, salt);
+
+    user.password = hash;
+
+    const newUser = await this.usersRepository.save(user);
+
+    return { id: newUser.id, username: newUser.username, email: newUser.email };
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async getUserById(id: number): Promise<User> {
+    return await this.usersRepository.findOneBy({ id });
+  }
+
+  //  updateUser() {}
+
+  async deleteUser(id: number): Promise<DeleteResult> {
+    return await this.usersRepository.delete(id);
+  }
+}
