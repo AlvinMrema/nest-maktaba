9f67509404eb94981026e6bdb0610f7e032e78dc
AlvinMrema <sonalpha023@gmail.com>
2023-03-03 15:47:39 +0300

    Added DB config with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { BookModule } from './modules/book/book.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/user.entity';
 
 @Module({
-  imports: [UserModule, BookModule, AuthModule],
+  imports: [
+    UserModule,
+    BookModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'alvin',
+      password: 'password',
+      database: 'library',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
